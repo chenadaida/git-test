defe8f92f3dc8cac8fca0daa71f85897472b2651
chensuilin <2227691886@qq.com>
2023-03-10 15:44:25 +0800

    master commit first

--- src/main/java/csl/git/git1.java
+++ src/main/java/csl/git/git1.java
@@ -6,5 +6,6 @@ public class git1 {
         System.out.println("hello.git");
         System.out.println("hello.git");
         System.out.println("hello hot-fix");
+        System.out.println("hello master1");
     }
 }
