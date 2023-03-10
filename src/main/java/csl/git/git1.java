063bdfc9b24b50e31ea46c0a44a2a0a9a67baa69
chensuilin <2227691886@qq.com>
2023-03-10 15:25:53 +0800

    third commit

--- src/main/java/csl/git/git1.java
+++ src/main/java/csl/git/git1.java
@@ -4,5 +4,6 @@ public class git1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello.git");
+        System.out.println("hello.git");
     }
 }
