eaea008e53c91506d0b4c4f56b6562cd0e2de844
chensuilin <2227691886@qq.com>
2023-03-10 15:31:44 +0800

    hot-fix commit first

--- src/main/java/csl/git/git1.java
+++ src/main/java/csl/git/git1.java
@@ -5,5 +5,6 @@ public class git1 {
         System.out.println("hello git");
         System.out.println("hello.git");
         System.out.println("hello.git");
+        System.out.println("hello hot-fix");
     }
 }
